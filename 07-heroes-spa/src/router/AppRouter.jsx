241872165777f12fda02e4483599f943e13e5bd0
luis carlos hernandez lopez <luis.hernandezlo@pragma.com.co>
2022-10-06 08:09:35 -0500

    inicio de aplicacion de heroes

---
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -0,0 +1,17 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { LoginPage } from "../auth/pages/LoginPage"
+import { DcPage } from "../heroes/pages/DcPage"
+import { MarvelPages } from "../heroes/pages/MarvelPages"
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/marvel" element={ <MarvelPages/> }/>
+                <Route path="/dc" element={ <DcPage/> }/>
+                <Route path="/login" element={ <LoginPage/> }/>
+                <Route path="/*" element={ <Navigate to="/marvel"/> }/>
+            </Routes>
+        </>
+    )    
+}
